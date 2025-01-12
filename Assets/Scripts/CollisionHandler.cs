91e9449f955005f8cd5fb2ca28e4b089b617587e
KimInJoong <ijboym216@gmail.com>
2025-01-12 10:57:03 +0900

    FEAT:SceneManager를 사용한 리스폰

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -20,8 +21,14 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Finish");
                 break;
             default:
-                Debug.Log("default");
+                ReloadLevel();
                 break;
         }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
