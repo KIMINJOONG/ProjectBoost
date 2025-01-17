b137f0cabb6151ae89721a31d6f4a158d1171e1f
KimInJoong <ijboym216@gmail.com>
2025-01-17 20:32:03 +0900

    FEAT:Mathf.Sin()함수 이용 물체 움직이기 추가

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movemntFactor;
+    [SerializeField] float period = 2f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startingPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movemntFactor = (rawSinWave - 1f) / 2f;
+        Vector3 offset = movementVector * movemntFactor;
+        transform.position = startingPosition + offset;
+    }
+}
