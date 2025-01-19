b7b5ae7e560d678db3f435010a2a31bd60465e46
KimInJoong <ijboym216@gmail.com>
2025-01-19 14:23:52 +0900

    FEAT:모바일용 버튼 추가

---
+++ Assets/Scripts/Boost.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Boost : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    bool isPressed = false;
+    [SerializeField] float mainThrust = 1000f;
+    public GameObject Rocket;
+    Rigidbody rb;
+    AudioSource audioSource;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem mainBooster;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Rocket = GameObject.Find("Rocket");
+
+        if (Rocket == null)
+        {
+            Debug.LogError("Player GameObject not found!");
+        }
+
+        if (Rocket != null)
+        {
+            // Rocket에서 Rigidbody를 접근
+            rb = Rocket.GetComponent<Rigidbody>();
+            audioSource = GetComponent<AudioSource>();
+            Transform mainBoosterTransform = Rocket.GetComponent<Transform>().Find("Rocket Jet Particles");
+            mainBooster = mainBoosterTransform.GetComponent<ParticleSystem>();
+
+
+
+            if (audioSource == null)
+            {
+                Debug.LogError("AudioSource component not found on Rocket!");
+            }
+
+            // AudioSource에서 AudioClip을 가져옵니다.
+            if (audioSource != null)
+            {
+                // mainEngine = audioSource.clip;  // AudioSource에서 현재 할당된 AudioClip을 가져옵니다.
+            }
+
+            if (mainEngine == null)
+            {
+                Debug.LogError("AudioClip not assigned in AudioSource on Rocket!");
+            }
+
+            if (rb == null)
+            {
+                Debug.LogError("Rigidbody not found on targetObject!");
+            }
+        }
+        else
+        {
+            Debug.LogError("targetObject is not assigned!");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            StartThrusting();
+        }
+        else
+        {
+            StopThrusting();
+
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+
+    void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        Debug.Log(audioSource);
+        if (!audioSource.isPlaying)
+        {
+            Debug.Log(mainEngine);
+            audioSource.PlayOneShot(mainEngine);
+        }
+
+        if (!mainBooster.isPlaying)
+        {
+            mainBooster.Play();
+        }
+    }
+
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        mainBooster.Stop();
+    }
+}
---
+++ Assets/Scripts/LeftMove.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LeftMove : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    bool isPressed = false;
+    public GameObject Rocket;
+    Rigidbody rb;
+    [SerializeField] ParticleSystem rightBooster;
+    [SerializeField] float rotationThrust = 100f;
+
+    void Start()
+    {
+        // "Player"라는 이름을 가진 GameObject를 찾습니다.
+        Rocket = GameObject.Find("Rocket");
+
+        if (Rocket == null)
+        {
+            Debug.LogError("Player GameObject not found!");
+        }
+
+        if (Rocket != null)
+        {
+            // Rocket에서 Rigidbody를 접근
+            rb = Rocket.GetComponent<Rigidbody>();
+
+            if (rb == null)
+            {
+                Debug.LogError("Rigidbody not found on targetObject!");
+            }
+        }
+        else
+        {
+            Debug.LogError("targetObject is not assigned!");
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            RotateRight();
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+
+    void RotateRight()
+    {
+        AapplyRotation(rotationThrust);
+        if (!rightBooster.isPlaying)
+        {
+            rightBooster.Play();
+        }
+    }
+
+    void AapplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // 수동제어를 할 수 있도록 회전을 고정한다.
+        Rocket.transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // 물리 시스템이 적용되도록 회전 고정을 해제한다.
+    }
+
+
+
+
+}
